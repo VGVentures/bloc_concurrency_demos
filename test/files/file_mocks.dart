1972f2d8b65f52c0ce9c975093e51d76caa87459
Joanna May <joannamadisonmay@gmail.com>
2021-10-26 14:25:58 -0500

    Finish files tests

--- test/files/file_mocks.dart
+++ test/files/file_mocks.dart
@@ -4,6 +4,6 @@ import 'package:bloc_concurrency_demos/files/file_repo.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockFileCubit extends MockCubit<FileState> implements FileCubit {}
+class MockFileBloc extends MockCubit<FileState> implements FileCubit {}
 
 class MockFileRepo extends Mock implements FileRepo {}
